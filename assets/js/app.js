0181c9a7b46691044fd0748f7870529f9bf72b18
Ran Golani <rango2001@gmail.com>
2024-12-07 16:51:14 +0200

    init project

---
+++ assets/js/app.js
@@ -0,0 +1,94 @@
+function collectData() {
+    const description = document.getElementById('description').value
+    const date = document.getElementById('date').value
+    const time = document.getElementById('time').value
+    const index = getNumberOfTasksInLocalStorage()
+    return {
+        index,
+        description,
+        date,
+        time,
+    }
+}
+
+function generateHTML(data) {
+    const newHTML = `
+        <div class="task">
+            <div>
+                <img src="assets/images/x.png" onclick="deleteTask(${data.index})">
+            </div>
+            <div>${data.description}</div>
+            <div>${data.date}</div>
+            <div>${data.time}</div>
+        </div>
+    `
+
+    return newHTML
+}
+
+function renderHTML(newHTML) {
+    const tasksContainer = document.getElementById('tasks')
+    tasksContainer.innerHTML += newHTML
+}
+
+function clearForm() {
+    const tasksForm = document.getElementById('tasksForm')
+    tasksForm.reset()
+
+    const descriptionInput = document.getElementById('description')
+    descriptionInput.focus()
+}
+
+function saveTaskToStorage(taskObject) {
+    // get JSON from local storage
+    const currentTasksInStorageJSON = localStorage.getItem('tasks')
+
+    // convert JSON to JavaScript object
+    const currentTasksInStorage = JSON.parse(currentTasksInStorageJSON)
+
+    // the object I got is an array, push another item to the array
+    currentTasksInStorage.push(taskObject)
+
+    // save it back to the local storage
+    localStorage.setItem('tasks', JSON.stringify(currentTasksInStorage))
+}
+
+function loadCarsFromLocalStorage() {
+    const carsJSON = localStorage.getItem('cars')
+    if(carsJSON) {
+        const cars = JSON.parse(carsJSON)
+        for(const car of cars) {
+            const newHTML = generateHTML(car)
+            renderHTML(newHTML)
+        }
+    }
+}
+
+function initStorage() {
+    const currentTasksInStorageJSON = localStorage.getItem('tasks')
+    if(!currentTasksInStorageJSON) {
+        localStorage.setItem('tasks', JSON.stringify([]))
+    }
+    
+}
+
+function getNumberOfTasksInLocalStorage() {
+    return JSON.parse(localStorage.getItem('tasks')).length
+}
+
+function addTask(event) {
+    event.preventDefault()
+    const data = collectData()
+    const newHTML = generateHTML(data)
+    renderHTML(newHTML)
+    saveTaskToStorage(data)
+    clearForm()
+}
+
+function deleteTask(index) {
+    alert(`will delete item #${index} from local storage`)
+}
+
+
+initStorage()
+loadCarsFromLocalStorage()
